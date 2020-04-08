de299bd22d14c2a1a2e1d85ee7e10f8b87356305
Colby Terry <colbydude@voidteam.net>
2020-04-08 00:52:24 -0700

    Setup basic game loop and SDL renderer/window.

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
+# @NOTE: NEED TO USE TABS, NOT SPACES
+
 build:
-	g++ -w -std=c++14 \
+	g++ -w -std=c++17 \
 	./src/*.cpp \
 	-o game \
 	-lSDL2 \
 	-lSDL2_image \
-	-lSDL2_ttf \
-	-lSDL2_mixer;
+	-lSDL2_mixer \
+	-lSDL2_ttf;
 
 clean:
 	rm ./game;
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
 
 int main(int argc, char *args[])
 {
-    std::cout << "Game is running..." << std::endl;
+    Game *game = new Game();
+
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while (game->IsRunning()) {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
 
     return 0;
 }
