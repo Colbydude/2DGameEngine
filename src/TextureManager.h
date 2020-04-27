1d3eb3a907793ff0e12374e11107b096cd35bea2
Colby Terry <colbydude@voidteam.net>
2020-04-27 00:12:37 -0700

    Asset managment WIP.

---
+++ src/TextureManager.h
@@ -0,0 +1,16 @@
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+#include "./Game.h"
+
+class TextureManager
+{
+    public:
+        static void Draw(
+            SDL_Texture* texture, SDL_Rect sourceRectangle,
+            SDL_Rect destinationRectangle, SDL_RendererFlip flip
+        );
+        static SDL_Texture* LoadTexture(const char* fileName);
+};
+
+#endif
