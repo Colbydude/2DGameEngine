4b40f228e83a57c3bd5b9374c9adebe3f965d1db
Colby Terry <colby.terry@slalom.com>
2020-05-20 19:41:01 -0700

    Set internal screen resolution and add scaling.

--- src/Constants.h
+++ src/Constants.h
@@ -1,7 +1,10 @@
 #pragma once
 
-const unsigned int WINDOW_WIDTH = 800;              /** Default window width. */
-const unsigned int WINDOW_HEIGHT = 600;             /** Default window height. */
+const unsigned int SCREEN_WIDTH = 256;              /** Default screen width (NES). */
+const unsigned int SCREEN_HEIGHT = 240;             /** Default screen height (NES). */
+
+const unsigned int WINDOW_WIDTH = 768;              /** Default window width. */
+const unsigned int WINDOW_HEIGHT = 720;             /** Default window height. */
 
 const unsigned int FPS = 60;                        /** Target frames per second. */
 const unsigned int FRAME_TARGET_TIME = 1000 / FPS;  /** Target frame time of 16ms. */
